6063f4db4dd010095e5079488094fa770379ba73
hwnoh99 <coelacenth@gmail.com>
2021-04-16 01:20:59 +0800

    node.js tree

--- app.js.js
+++ app.js.js
@@ -6,19 +6,6 @@ class Node {
     }
 } 
 
-const rootNode = new Node(0);
-rootNode.children.push(new Node(10));
-rootNode.children.push(new Node(20));
-rootNode.children.push(new Node(30));
-rootNode.children[0].children.push(new Node(40));
-rootNode.children[0].children.push(new Node(50));
-rootNode.children[2].children.push(new Node(60));
-
-setRight(rootNode);
-setRight(rootNode.children[0]);
-setRight(rootNode.children[1]);
-setRight(rootNode.children[2]);
-
 function setRight(node){
     if (node.children.length != 0) {
         for (i=0; i<node.children.length-1; i++) {
@@ -35,21 +22,21 @@ function setRight(node){
     }
 }
 
-function search_UntilNext(node) {
-    if (node.right === null) {
-        return;
-    }
+//testing the creation of the Tree 
+const rootNode = new Node(0);
+rootNode.children.push(new Node(10));
+rootNode.children.push(new Node(20));
+rootNode.children.push(new Node(30));
+rootNode.children[0].children.push(new Node(40));
+rootNode.children[0].children.push(new Node(50));
+rootNode.children[2].children.push(new Node(60));
 
-    if (node.right.children.length === 0){
-        search_UntilNext(node.right);
-    }
-    else {
-        let i = node.right.children.length-1;
-        node.children[i] = node.right.children[0].id;
-    }
-    
-}
+setRight(rootNode);
+setRight(rootNode.children[0]);
+setRight(rootNode.children[1]);
+setRight(rootNode.children[2]);
 
+//print the test the right value of each node
 console.log(rootNode.children[0].right);
 console.log(rootNode.children[1].right);
 console.log(rootNode.children[2].right);
